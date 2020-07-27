a354ca57d9fc48d76a5e906d0bdaf26efebede22
Sameer <sameer.w@somaiya.edu>
2020-07-27 14:18:37 +0530

    Internship Flutter expense project

---
+++ lib/models/popupmenuitems.dart
@@ -0,0 +1,6 @@
+class PopupMenuItems {
+  static const String debit = 'Debit Money';
+  static const String credit = 'Credit Money';
+
+  static const List<String> choices = <String>[debit, credit];
+}
