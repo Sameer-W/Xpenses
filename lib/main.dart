feeba7eacc4bf3753299a732646f4711b672bcdc
Sameer-W <60566843+Sameer-W@users.noreply.github.com>
2020-07-29 12:53:20 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:./models/popupmenuitems.dart';
-import 'package:./services/firestore-crud.dart';
-import 'package:./widgets/history-transactions.dart';
+import 'package:../models/popupmenuitems.dart';
+import 'package:../services/firestore-crud.dart';
+import 'package:../widgets/history-transactions.dart';
 
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
